0846eb3d7742cdaedecdda8c816da27bf30c6612
xiong19211 <xiong19211@gmail.com>
2021-02-22 21:18:59 +0800

    更新shoppingMall Controller

--- src/main/java/_22_shoppingCart/controller/ShoppingCartController.java
+++ src/main/java/_22_shoppingCart/controller/ShoppingCartController.java
@@ -21,4 +21,34 @@ public class ShoppingCartController {
 		return "_21_shoppingMall/MerchandiseSearchResult";
 	}
 	
+	@RequestMapping("MerchandiseDetail")
+	public String getMerchandiseDetail() {
+		return "_21_shoppingMall/MerchandiseDetail";
+	}
+	
+	@RequestMapping("ShoppingCart")
+	public String getShoppingCart() {
+		return "_21_shoppingMall/ShoppingCart";
+	}
+	
+	@RequestMapping("SelectPayment")
+	public String getSelectPayment() {
+		return "_21_shoppingMall/SelectPayment";
+	}
+	
+	@RequestMapping("PurchaseSuccess")
+	public String getPurchaseSuccess() {
+		return "_21_shoppingMall/PurchaseSuccess";
+	}
+	
+	@RequestMapping("MyOrderList")
+	public String getMyOrderList() {
+		return "_21_shoppingMall/MyOrderList";
+	}
+	
+	@RequestMapping("MerchandiseIndex")
+	public String getMerchandiseIndex() {
+		return "_21_shoppingMall/MerchandiseIndex";
+	}
+	
 }
