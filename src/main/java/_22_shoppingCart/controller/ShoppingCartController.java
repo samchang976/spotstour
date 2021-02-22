33767fe651ae66e14d99acda01f4b87f0987a6ad
xiong19211 <xiong19211@gmail.com>
2021-02-22 19:32:00 +0800

    增加ActivityDetail部分設定

--- src/main/java/_22_shoppingCart/controller/ShoppingCartController.java
+++ src/main/java/_22_shoppingCart/controller/ShoppingCartController.java
@@ -10,4 +10,15 @@ public class ShoppingCartController {
 	public String shoppingCart() {
 		return "_21_shoppingMall/ShoppingCart";
 	}
+	
+	@RequestMapping("/ActivityDetail")
+	public String getActivityDetail() {
+		return "_21_shoppingMall/ActivityDetail";
+	}
+	
+	@RequestMapping("MerchandiseSearchResult")
+	public String getSearchResult() {
+		return "_21_shoppingMall/MerchandiseSearchResult";
+	}
+	
 }
