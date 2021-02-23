5d4a8104c74db859d9e84639bed02159503b0ba8
xiong19211 <xiong19211@gmail.com>
2021-02-23 23:28:36 +0800

    增加_37_portfolioManage套件、CollectVideo()、PersonalVideo(建置中) 設定portfolioManageController

---
+++ src/main/java/_37_portfolioManage/controller/PortfolioManageController.java
@@ -0,0 +1,20 @@
+package _37_portfolioManage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PortfolioManageController {
+	@RequestMapping("personalVideo")
+	public String getPersonalVideo() {
+		return "_31_portfolio/PersonalVideo";
+	}
+	
+	@RequestMapping("collectVideo")
+	public String getCollectVideo() {
+		return "_31_portfolio/CollectVideo";
+	}
+	
+	
+	
+}
---
+++ src/main/webapp/WEB-INF/views/_31_portfolio/PersonalVideo.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>個人作品</title>
+</head>
+<body>
+
+網頁建置中~~~
+
+</body>
+</html>
