bab87cfcef767e16fb2b9b0e23df84203e8d53ce
xiong19211 <xiong19211@gmail.com>
2021-02-24 18:11:43 +0800

    change password:0000

--- src/main/java/_01_config/RootAppConfig.java
+++ src/main/java/_01_config/RootAppConfig.java
@@ -23,7 +23,7 @@ public class RootAppConfig {
 		ComboPooledDataSource ds = new ComboPooledDataSource();
 		//請變更自定義資料庫帳號密碼
 		ds.setUser("root");
-		ds.setPassword("JonGy9299");
+		ds.setPassword("0000");
 		try {ds.setDriverClass("com.mysql.cj.jdbc.Driver");
 			
 		} catch (Exception e) {
