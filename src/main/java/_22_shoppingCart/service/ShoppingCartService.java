4c4db7c9708e88f9f5107cf9ccf5571f42299bd1
xiong19211 <xiong19211@gmail.com>
2021-03-05 19:22:09 +0800

    增加 加入購物車的form區塊

---
+++ src/main/java/_22_shoppingCart/service/ShoppingCartService.java
@@ -0,0 +1,16 @@
+package _22_shoppingCart.service;
+
+import java.util.List;
+
+import _02_model.entity.ShoppingCartBean;
+
+public interface ShoppingCartService {
+	public void saveShoppingCart(ShoppingCartBean cart);//新增一筆商品到購物車
+	
+	public void deleteItem(int sc_Id);//刪除購物車中的一筆商品
+	
+	public  void UpdateQty(ShoppingCartBean cart);//修改購物車一筆商品數量
+	
+	public List<Object> getShoppingCart(int memberId); //由memberId取得購物車清單:有順序性，先加的要在上面
+
+}
