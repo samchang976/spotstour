d7b0edd2fcf6b3d251476e8e5dce4172dc8a912f
xiong19211 <xiong19211@gmail.com>
2021-03-15 20:00:14 +0800

    增加 sessionsShoppingCart

--- src/main/java/_22_shoppingCart/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/_22_shoppingCart/service/impl/ShoppingCartServiceImpl.java
@@ -1,6 +1,10 @@
 package _22_shoppingCart.service.impl;
 
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +16,7 @@ import _02_model.entity.MemberBean;
 import _02_model.entity.ShoppingCartBean;
 import _22_shoppingCart.dao.shoppingCartDao;
 import _22_shoppingCart.service.ShoppingCartService;
+import _22_shoppingCart.vo.SessionShoppingCartVo;
 
 @Transactional
 @Service // Service方法:企業邏輯，定義交易
@@ -80,29 +85,84 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 	public String addToCart(Integer memberId, Integer itemId, Integer qty) {
 //		try {
 		System.out.println("addcart-Service 開始==================");
-			// 判斷購物車某會員是否已經加入某商品至購物車
-			ShoppingCartBean shoppingCartBean = shoppingCartDao.hasCart(memberId, itemId);
-			if (shoppingCartBean == null) {
+		// 判斷購物車某會員是否已經加入某商品至購物車
+		ShoppingCartBean shoppingCartBean = shoppingCartDao.hasCart(memberId, itemId);
+		if (shoppingCartBean == null) {
 
-				shoppingCartBean = new ShoppingCartBean();
-				shoppingCartBean.setS_ordQty(qty);
-				shoppingCartBean.setItemBean(shoppingCartDao.getItemBeanByItemId(itemId));
-				shoppingCartBean.setMemberBean(shoppingCartDao.getMemberBeanBymId(memberId));
-				
-				shoppingCartDao.addShoppingCart(shoppingCartBean);
-				return "0";
+			shoppingCartBean = new ShoppingCartBean();
+			shoppingCartBean.setS_ordQty(qty);
+			shoppingCartBean.setItemBean(shoppingCartDao.getItemBeanByItemId(itemId));
+			shoppingCartBean.setMemberBean(shoppingCartDao.getMemberBeanBymId(memberId));
 
-			} else { // 購物車裡面已經存在會員id 與 產品id
-				shoppingCartBean.setS_ordQty(shoppingCartBean.getS_ordQty() + qty); // 傳來的數量加上最原本的
-				System.out.println("addcart-Service 完成==================");
-				return "1";
-			}
+			shoppingCartDao.addShoppingCart(shoppingCartBean);
+			return "0";
+
+		} else { // 購物車裡面已經存在會員id 與 產品id
+			shoppingCartBean.setS_ordQty(shoppingCartBean.getS_ordQty() + qty); // 傳來的數量加上最原本的
+			System.out.println("addcart-Service 完成==================");
+			return "1";
+		}
 //
 //			// 創建購物車
 //		} catch (SQLException e) {
 //			e.printStackTrace();
 //		}
 
+	}
+
+	@Transactional
+	@SuppressWarnings("null")
+	@Override
+	public List<SessionShoppingCartVo> getShoppingCartVo(Map<Integer, Integer> cartlist) {
+
+//		// 如果找不到ShoppingCart清單
+//		if (cartlist == null) {
+//			cartlist = new LinkedHashMap<>();
+//			if (itemQty == null) {
+//				cartlist.put(itemId, 1);
+//			} else {
+//				cartlist.put(itemId, itemQty);
+//			}
+//		} else {
+//			if (itemQty == null) {
+//				cartlist.put(itemId, 1);
+//			} else {
+//				cartlist.put(itemId, itemQty);
+//			}
+//		}
+
+		
+		 Integer key = null;
+		 List<SessionShoppingCartVo> sscList = new LinkedList<>();
+		
+		 Iterator<Integer> iter = cartlist.keySet().iterator();
+		  while(iter.hasNext()){
+			  key=iter.next();
+			  Integer value = cartlist.get(key);
+		   System.out.println("keyvalue==========="+key+" : "+value);
+		
+		
+//		int i;
+
+//		for (i = 0; i < (cartlist.size() - 1); i++) {
+			SessionShoppingCartVo ssc = new SessionShoppingCartVo();
+			ItemBean newItemBean = shoppingCartDao.getItemBeanByItemId(key);
+
+			System.out.println("newItemBean=========" + newItemBean);
+			ssc.setItemHeader(newItemBean.getItemHeader());
+			ssc.setItemPrice(newItemBean.getItemPrice());
+			ssc.setItemPic1(newItemBean.getItemPic1());
+			ssc.setScQty(value);
+
+			sscList.add(ssc);
+//		}
+		  }
+		System.out.println("cartlistSE===================="+cartlist);
+		System.out.println("sscListSE================="+sscList);
+		
+		return sscList;
+		
+//		return null;
 	}
 
 }
