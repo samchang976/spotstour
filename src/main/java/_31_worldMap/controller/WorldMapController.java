f3694018bdf5f68ee232d5e8c0b516ad1cf91d35
cheying306 <cheying12000@gmail.com>
2021-03-25 05:37:02 +0800

    補上世界地圖需要的地點類型LIST

--- src/main/java/_31_worldMap/controller/WorldMapController.java
+++ src/main/java/_31_worldMap/controller/WorldMapController.java
@@ -8,17 +8,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import _31_worldMap.service.WorldMapService;
+import _37_portfolioManage.service.GetPlace_TypeListService;
 
 @Controller
 public class WorldMapController {
 	
 	@Autowired
 	private WorldMapService worldMapService;
+	@Autowired
+	private GetPlace_TypeListService getPlace_TypeListService;
 	
 	@RequestMapping("/worldMap")
 	public String worldMap(Model model,HttpSession session) {
-		//於畫面上使用"allPortfolioList"識別字串,取出對應的值
+		//於畫面上使用"allPortfolioList"識別字串,取出所有作品欄位對應的值
 		model.addAttribute("allPortfolioList", worldMapService.getAllPortfolioList());
+		//於畫面上使用"placeTypeList"識別字串,取出所有地點類型欄位對應的值
+		model.addAttribute("placeTypeList", getPlace_TypeListService.getPlace_TypeList());
+		
 		return "_31_portfolio/WorldMap";
 	}
 	
